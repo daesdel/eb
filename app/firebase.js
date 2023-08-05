c07c53878c14e40f8f0e9340636ee8305fbe8cb9
daesdel <76701095+daesdel@users.noreply.github.com>
2023-08-05 13:21:29 +0200

    Add files via upload
    Cambio web a ejemplo con fireApp

---
+++ app/firebase.js
@@ -0,0 +1,21 @@
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+    import { getAuth } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+    import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js"
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyCxltcLjC6IpF0cC74g8zI570Q3miaFbyQ",
+      authDomain: "todo-b5800.firebaseapp.com",
+      projectId: "todo-b5800",
+      storageBucket: "todo-b5800.appspot.com",
+      messagingSenderId: "224494523333",
+      appId: "1:224494523333:web:0b36dd4fcd4604711271ac"
+    };
+  
+    // Initialize Firebase
+    export const app = initializeApp(firebaseConfig);
+    export const auth = getAuth (app);
+    export const firestore = getFirestore (app)
---
+++ app/signupForm.js
@@ -0,0 +1,31 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { auth } from './firebase.js'
+
+const signupForm = document.querySelector('#signup-form')
+signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const email = signupForm['signup-email'].value
+    const password = signupForm['signup-password'].value
+    console.log(email,password)
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        console.log(userCredentials)
+        const signupModal = document.querySelector('#signupModal')
+        const modal = bootstrap.Modal.getInstance(signupModal)
+        modal.hide()
+    } catch (error) {
+        console.log (error.code)
+
+        if (error.code === 'auth/email-already-in-use') {
+            alert ('El usuario ya está registrado')
+        } if (error.code === 'auth/invalid-email') {
+            alert ('mail incorrecto')
+          } else if (error.code === 'auth/weak-password') {
+            alert ('Password demasiado corto')
+            }
+        
+
+
+    }
+
+})
