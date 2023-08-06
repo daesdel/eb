1a988af4021587c15083ba73ea277a3a8e9d1aaa
daesdel <daesdel@gmail.com>
2023-08-06 13:25:47 +0200

    a

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { auth } from './app/firebase.js'
 import './app/signupForm.js'
+import './app/logout.js'
+
+onAuthStateChanged (auth, async (user) => {
+  console.log(user)
+})
