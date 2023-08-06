361e9600df855b7b8c6dc6610bc9360e502a805a
daesdel <76701095+daesdel@users.noreply.github.com>
2023-08-06 10:21:34 +0200

    2

--- app/signupForm.js
+++ app/signupForm.js
@@ -25,7 +25,7 @@ signupForm.addEventListener('submit', async (e) => {
             position: "right", // `left`, `center` or `right`
             stopOnFocus: true, // Prevents dismissing of toast on hover
             style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
+              background: 'green', //"linear-gradient(to right, #00b09b, #96c93d)",
             },
             onClick: function(){} // Callback after click
           }).showToast();
