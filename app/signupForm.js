d14685678180d96892a0358a8672117575d64c79
daesdel <76701095+daesdel@users.noreply.github.com>
2023-08-06 10:17:44 +0200

    Añado Toastity en prueba

--- app/signupForm.js
+++ app/signupForm.js
@@ -13,6 +13,25 @@ signupForm.addEventListener('submit', async (e) => {
         const signupModal = document.querySelector('#signupModal')
         const modal = bootstrap.Modal.getInstance(signupModal)
         modal.hide()
+
+        // Toastity
+        Toastify({
+            text: "This is a toast",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+
+
+
     } catch (error) {
         console.log (error.code)
 
