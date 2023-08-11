844836efd94c57af104c09b3271e2977f3efddd9
daesdel <76701095+daesdel@users.noreply.github.com>
2023-08-11 11:46:21 +0200

    mensaje acta ericsson enviada

--- app/entregaActaE.js
+++ app/entregaActaE.js
@@ -1,23 +1,30 @@
 const button = document.getElementById("button-actaE");
 button.addEventListener('click', async () => {
-    await showForm()
-    console.log ('FORMULARIO MOSTRADO')
-    
-})
+    await showForm();
+    console.log('FORMULARIO MOSTRADO');
+});
+
 function showForm() {
     // Mostrar el formulario
-    document.getElementById("form-actaE").style.display = "block"
-    console.log ('FORMULARIO ACTIVO')
+    document.getElementById("form-actaE").style.display = "block";
+    console.log('FORMULARIO ACTIVO');
 }
 
-const form = document.querySelector('#form-actaE')
+const form = document.querySelector('#form-actaE');
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Obtener los valores de los campos de texto
-  const escenario = form['escenario'].value
-  const IRJira = form['IRJira'].value
-  // Enviar los valores de los campos de texto a la consola
-  console.log("Enviado:", escenario, "  ", IRJira);
-  location.reload();
+    // Obtener los valores de los campos de texto
+    const escenario = form['escenario'].value;
+    const IRJira = form['IRJira'].value;
+    // Enviar los valores de los campos de texto a la consola
+    console.log("Enviado:", escenario, "  ", IRJira);
+
+    // Mostrar el cuadro emergente "Formulario enviado"
+    alert("Formulario enviado");
+
+    // Esperar 2 segundos (2000 milisegundos) antes de recargar la página
+    setTimeout(() => {
+        location.reload();
+    }, 2000);
 });
