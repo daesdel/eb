688bc4e7a9e7d3ec5e2e096cca129591370015ac
daesdel <daesdel@gmail.com>
2023-08-24 17:53:06 +0200

    guardar en BD

--- app/entregaActaE.js
+++ app/entregaActaE.js
@@ -1,22 +1,33 @@
+import { showMessage } from './showMessage.js';
+import { guardarDocumento } from './firebase.js'
+
 const button = document.getElementById("button-actaE");
 button.addEventListener('click', async () => {
-    await showForm()
-    console.log ('FORMULARIO MOSTRADO')
-    
-})
+    await showForm();
+    console.log('FORMULARIO MOSTRADO');
+});
+
 function showForm() {
     // Mostrar el formulario
-    document.getElementById("form-actaE").style.display = "block"
-    console.log ('FORMULARIO ACTIVO')
+    document.getElementById("form-actaE").style.display = "block";
+    console.log('FORMULARIO ACTIVO');
 }
 
-const form = document.querySelector('#form-actaE')
+const form = document.querySelector('#form-actaE');
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Obtener los valores de los campos de texto
-  const nombre = form['nombre'].value
-  // Enviar los valores de los campos de texto a la consola
-  console.log("Enviado:", nombre);
-  location.reload();
+    // Obtener los valores de los campos de texto
+    const escenario = form['escenario'].value;
+    const IRJira = form['IRJira'].value;
+    const CodSite = form['CodSite'].value;
+
+    guardarDocumento ("Acta",IRJira,CodSite,escenario)
+
+    // Enviar los valores de los campos de texto a la consola
+    console.log("Enviado:", escenario, "  ", IRJira);
+
+    // Mostrar el cuadro emergente 
+    showMessage ("Acta enviada " + IRJira, "success");
+    form.reset()
 });
--- app/firebase.js
+++ app/firebase.js
@@ -1,7 +1,7 @@
     // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
     import { getAuth } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-    import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js"
+    import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js"
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
   
@@ -19,4 +19,8 @@
     // Initialize Firebase
     export const app = initializeApp(firebaseConfig);
     export const auth = getAuth (app);
-    export const firestore = getFirestore (app)
+    export const db = getFirestore ()
+    
+    export const guardarDocumento = (documento, IRJira, CodSite, escenario) => {
+      addDoc (collection (db, documento), {IRJira, CodSite, escenario});
+    }
